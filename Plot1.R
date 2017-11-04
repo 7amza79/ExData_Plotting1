7f6f14af9adedb4c5384e10e417ad970b5a60740
7amza79 <ben.marzouk.hamza@gmail.com>
2017-11-04 17:58:56 +0100

    Exercice 1 : code & png

---
+++ Plot1.R
@@ -0,0 +1,26 @@
+#download data file & unzip it in workspace
+
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="data.zip", method="curl")
+unzip ("data.zip", exdir = "./")
+
+#read data 
+
+power <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+
+# transform date field
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+
+# get the required time interval
+
+power <- power[power$Date >= as.Date("2007-02-01") & power$Date <= as.Date("2007-02-02"),]
+
+# create the png file for the plot
+
+png(file = "plot1.png", width = 480, height = 480, units = "px")
+
+# create the histogram 
+hist(power$Global_active_power, col="red", main= "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+# turn off the graphic device
+
+dev.off()
